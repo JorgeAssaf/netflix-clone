bd8a252990b3e1aeaac92a415a883c98fb7c697e
Jorge Assaf <jorgeassaf160@gmail.com>
2023-06-23 21:26:41 -0600

    fixed metadata

--- src/app/(watch)/watch/[movieId]/layout.tsx
+++ src/app/(watch)/watch/[movieId]/layout.tsx
@@ -1,26 +1,4 @@
-import { Metadata, ResolvingMetadata } from 'next'
 
-interface Props {
-  params: { movieId: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent?: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const movieId = params.movieId
-  console.log(movieId)
-  // fetch data
-  const movie = await fetch(`netflix-clone-tau-opal.vercel.app/api/movies/${movieId}`)
-  const res = await movie.json()
-  console.log(res)
-
-  return {
-    title: res?.title || 'Movie'
-  }
-}
 export default function watchLayout({
   children
 }: {
